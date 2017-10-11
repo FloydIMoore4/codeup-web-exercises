6d2051cae58f4e16486f5d293e11705e9a735e78
floydmoore <floydimoore@yahoo.com>
2017-10-11 16:35:26 -0500

    added images

--- js/external.js
+++ js/external.js
@@ -1,4 +1,6 @@
-console.log("hello from javascript");
+"use strict"
+console.log("hello from external javascript");
 alert ("welcome to my website");
-var whatColor = prompt ("favorite color");
-prompt ("what color");
+var color = prompt ("What is your favorite color");
+alert ("+ color + is my favorite color too!");
+
